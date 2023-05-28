da9b930a92ef10e39e00e0ef2a138485895a4d2d
ctatis1 <ctatis1@users.noreply.github.com>
2023-05-28 20:59:58 +0000

    deploy: 59067fdb4d68ec60c27254d3ea96c86da032e2c6

--- docs/photomosaic/photomosaic.js
+++ docs/photomosaic/photomosaic.js
@@ -18,7 +18,7 @@ function setup() {
   slider = createSlider(1, 6, 2,1);
   slider.position(50, 60);
   slider.style('width', '100px');
-  createCanvas(800, 600, WEBGL);
+  createCanvas(700, 600, WEBGL);
   textureMode(NORMAL);
   noStroke();
   shader(mosaic);
