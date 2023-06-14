c05beb72e8570471d933369c2026b008776b0440
ctatis1 <ctatis1@users.noreply.github.com>
2023-06-14 03:40:37 +0000

    deploy: 2d0f37165f97032912510fe41d028cebf1cbb787

--- docs/photomosaic/photomosaic.js
+++ docs/photomosaic/photomosaic.js
@@ -1,66 +1,86 @@
 var mocSize;
-var image;
+var img;
+var imgs = [];
+var size = 600;
+var lumaValues = [];
 var modeSelect;
 var mode;
-
 let p = 0;
-let size = 600;
-var images = [];
-var lumaValues = [];
 
 function preload() {
   for (i = 0; i < 30; i++) {
-    let newImage = loadImage(`/showcase/docs/photomosaic/assets/photo${i + 1}.jpg`);
-    images.push(newImage);
+    let imgNew = loadImage(`/showcase/docs/photomosaic/assets/photo${i + 1}.jpg`);
+    imgs.push(imgNew);
   }
-  image = loadImage("/showcase/docs/photomosaic/assets/avtar.jpg");
+  img = loadImage(`/showcase/docs/photomosaic/assets/avtar.jpg`);
 }
 
 function setup() {
   createCanvas(size, size);
-  image.loadPixels();
-  image.resize(size, size);
+  img.loadPixels();
+  img.resize(size, size);
   for (i = 0; i < 30; i++) {
-    images[i].loadPixels();
+    imgs[i].loadPixels();
     let sum = 0;
-    for (j = 0; j < images[i].pixels.length; j += 4) {
-      let r = images[i].pixels[j];
-      let g = images[i].pixels[j + 1];
-      let b = images[i].pixels[j + 2];
-      let a = images[i].pixels[j + 3];
+    for (j = 0; j < imgs[i].pixels.length; j += 4) {
+      let r = imgs[i].pixels[j];
+      let g = imgs[i].pixels[j + 1];
+      let b = imgs[i].pixels[j + 2];
+      let a = imgs[i].pixels[j + 3];
       sum += luma(r, g, b);
     }
-    let avg = sum / (images[i].pixels.length / 4);
+    let avg = sum / (imgs[i].pixels.length / 4);
     lumaValues.push(avg);
-    images[i].resize(size, size);
+    imgs[i].resize(size, size);
   }
   console.log(lumaValues);
   mocSize = createSlider(3, 100, 10, 1);
   mocSize.position(10, 10);
   mocSize.style("width", "80px");
-
+  modeSelect = createSelect();
+  modeSelect.position(10, 30);
+  modeSelect.option("Original");
+  modeSelect.option("Keys");
+  modeSelect.option("Symbols");
+  modeSelect.selected("Original");
 }
 
 function draw() {
   background(0);
-  for (i = 0; i < size; i += mocSize.value()) {
-    for (j = 0; j < size; j += mocSize.value()) {
-      let index = (j * size + i) * 4;
-      let r = image.pixels[index];
-      let g = image.pixels[index + 1];
-      let b = image.pixels[index + 2];
-      let c = color(r, g, b);
-
-      let bValue = luma(r, g, b);
-      let diff = Math.abs(lumaValues[0] - bValue);
-      let imageIndex = 0;
-      for (k = 1; k < lumaValues.length; k++) {
-        if (diff > Math.abs(lumaValues[k] - bValue)) {
-          imageIndex = k;
-          diff = Math.abs(lumaValues[k] - bValue);
+  mode = modeSelect.value();
+  if (mode == "Original") {
+    image(img, 0, 0);
+  } else {
+    for (i = 0; i < size; i += mocSize.value()) {
+      for (j = 0; j < size; j += mocSize.value()) {
+        let index = (j * size + i) * 4;
+        let r = img.pixels[index];
+        let g = img.pixels[index + 1];
+        let b = img.pixels[index + 2];
+        let c = color(r, g, b);
+        if (mode == "Symbols") {
+          let bValue = luma(r, g, b);
+          let diff = Math.abs(lumaValues[0] - bValue);
+          let imgIndex = 0;
+          for (k = 1; k < lumaValues.length; k++) {
+            if (diff > Math.abs(lumaValues[k] - bValue)) {
+              imgIndex = k;
+              diff = Math.abs(lumaValues[k] - bValue);
+            }
+          }
+          image(
+            imgs[imgIndex],
+            i,
+            j,
+            Math.min(mocSize.value(), size - i),
+            Math.min(mocSize.value(), size - j)
+          );
+        } else {
+          fill(c);
+          noStroke();
+          rect(i, j, mocSize.value(), mocSize.value());
         }
       }
-      image(images[imageIndex], i, j, Math.min(mocSize.value(), size - i), Math.min(mocSize.value(), size - j));
     }
   }
 }
@@ -68,8 +88,8 @@ function draw() {
 function keyPressed() {
   if (key == "r") {
     p = (p + 1) % 30;
-    image.copy(images[p], 0, 0, size, size, 0, 0, size, size);
-    image.loadPixels();
+    img.copy(imgs[p], 0, 0, size, size, 0, 0, size, size);
+    img.loadPixels();
   }
 }
 
