78fa82cfae15b2b7da477f0b696149cbad5ebd1f
ctatis1 <ctatis1@users.noreply.github.com>
2023-06-14 03:23:00 +0000

    deploy: 65bc625ba55b89e08213473a69c85ef18405c92d

--- docs/photomosaic/photomosaic.js
+++ docs/photomosaic/photomosaic.js
@@ -1,65 +1,78 @@
-let mosaic;
-let symbol1;
-let myImage;
-let debug;
-let slider;
-const WIDTH_PIXEL = 64;
-const HEIGHT_PIXEL = 64;
-const NUM_IMAGES = 99;
+var mocSize;
+var image;
+var modeSelect;
+var mode;
+
+let p = 0;
+let size = 600;
+var images = [];
+var lumaValues = [];
+
 function preload() {
-  myImage = loadImage("/showcase/docs/photomosaic/avtar.jpg");
-  symbol1 = loadImage("/showcase/docs/photomosaic/dataset.png");
-  mosaic = loadShader(
-    "/showcase/docs/photomosaic/shader.vert",
-    "/showcase/docs/photomosaic/photomosaic.frag" 
-  );
+  for (i = 0; i < 30; i++) {
+    let newImage = loadImage(`/showcase/docs/photomosaic/assets/photo${i + 1}.jpg`);
+    images.push(newImage);
+  }
+  image = loadImage("/showcase/docs/photomosaic/assets/avtar.jpg");
 }
 
 function setup() {
-  slider = createSlider(1, 6, 2, 1);
-  slider.position(50, 60);
-  slider.style('width', '100px');
-  createCanvas(700, 600, WEBGL);
-  textureMode(NORMAL);
-  noStroke();
-  shader(mosaic);
-  mosaic.setUniform("image", myImage);
-  mosaic.setUniform("WIDTH_PIXEL", WIDTH_PIXEL);
-  mosaic.setUniform("NUM_IMAGES", NUM_IMAGES);
-  mosaic.setUniform("HEIGHT_PIXEL", HEIGHT_PIXEL);
-  debug = true;
-  mosaic.setUniform("debug", debug);
-  let img = symbol1;
-  mosaic.setUniform("symbol1", img);
+  createCanvas(size, size);
+  image.loadPixels();
+  image.resize(size, size);
+  for (i = 0; i < 30; i++) {
+    images[i].loadPixels();
+    let sum = 0;
+    for (j = 0; j < images[i].pixels.length; j += 4) {
+      let r = images[i].pixels[j];
+      let g = images[i].pixels[j + 1];
+      let b = images[i].pixels[j + 2];
+      let a = images[i].pixels[j + 3];
+      sum += luma(r, g, b);
+    }
+    let avg = sum / (images[i].pixels.length / 4);
+    lumaValues.push(avg);
+    images[i].resize(size, size);
+  }
+  console.log(lumaValues);
+  mocSize = createSlider(3, 100, 10, 1);
+  mocSize.position(10, 10);
+  mocSize.style("width", "80px");
+
 }
 
 function draw() {
-  mosaic.setUniform("resolution", Math.pow(10,slider.value()));
+  background(0);
+  for (i = 0; i < size; i += mocSize.value()) {
+    for (j = 0; j < size; j += mocSize.value()) {
+      let index = (j * size + i) * 4;
+      let r = image.pixels[index];
+      let g = image.pixels[index + 1];
+      let b = image.pixels[index + 2];
+      let c = color(r, g, b);
 
-  background(33);
-  cover(true);
-}
-
-function cover(texture = false) {
-  beginShape();
-  if (texture) {
-    //texture(img);
-    vertex(-width / 2, -height / 2, 0, 0, 0);
-    vertex(width / 2, -height / 2, 0, 1, 0);
-    vertex(width / 2, height / 2, 0, 1, 1);
-    vertex(-width / 2, height / 2, 0, 0, 1);
-  } else {
-    vertex(-width / 2, -height / 2, 0);
-    vertex(width / 2, -height / 2, 0);
-    vertex(width / 2, height / 2, 0);
-    vertex(-width / 2, height / 2, 0);
+      let bValue = luma(r, g, b);
+      let diff = Math.abs(lumaValues[0] - bValue);
+      let imageIndex = 0;
+      for (k = 1; k < lumaValues.length; k++) {
+        if (diff > Math.abs(lumaValues[k] - bValue)) {
+          imageIndex = k;
+          diff = Math.abs(lumaValues[k] - bValue);
+        }
+      }
+      image(images[imageIndex], i, j, Math.min(mocSize.value(), size - i), Math.min(mocSize.value(), size - j));
+    }
   }
-  endShape(CLOSE);
 }
 
 function keyPressed() {
-  if (key === "z") {
-    debug = !debug;
-    mosaic.setUniform("debug", debug);
+  if (key == "r") {
+    p = (p + 1) % 30;
+    image.copy(images[p], 0, 0, size, size, 0, 0, size, size);
+    image.loadPixels();
   }
+}
+
+function luma(r, g, b) {
+  return 0.299 * r + 0.587 * g + 0.114 * b;
 }
