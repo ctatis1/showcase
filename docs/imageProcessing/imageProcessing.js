a9918bd4d6b52d030ed264c19e0d3e19056fb9d5
ctatis1 <ctatis1@users.noreply.github.com>
2023-06-19 04:30:30 +0000

    deploy: 021044eeae1ef203b3a2f1a96180effb1023fa0c

--- docs/imageProcessing/imageProcessing.js
+++ docs/imageProcessing/imageProcessing.js
@@ -9,78 +9,66 @@ let magnifier_checkbox;
 let radius_slider;
 
 function preload() {
-  maskShader = readShader('/showcase/docs/imageProcessing/mask.frag',
-                        { varyings: Tree.texcoords2 });
-  // Load image and video files
+  maskShader = readShader('/showcase/docs/imageProcessing/mask.frag',{ varyings: Tree.texcoords2 });
   img = loadImage('/showcase/docs/imageProcessing/Lego.jpg');
 }
 
 function setup() {
   createCanvas(640, 640, WEBGL);
   noStroke();
-  // Set up video and hide it HTML element
-  // Set up the shader
+
   textureMode(NORMAL);
   shader(maskShader);
-  // Set up the checkboxes and slider
-  mask_checkbox = createCheckbox('mask', false);
-  mask_checkbox.position(10, 30);
-  mask_checkbox.style('color', 'white');
-  mask_checkbox.input(uniformUpdate);
-  luma_checkbox = createCheckbox('luma', false);
-  luma_checkbox.position(10, 50);
-  luma_checkbox.style('color', 'white');
-  luma_checkbox.input(uniformUpdate);
-  roi_checkbox = createCheckbox('Region of interest', false);
-  roi_checkbox.position(10, 70);
-  roi_checkbox.style('color', 'white');
-  roi_checkbox.input(uniformUpdate);
+
   magnifier_checkbox = createCheckbox('Magnifier', false);
-  magnifier_checkbox.position(10, 90);
+  magnifier_checkbox.position(10, 30);
   magnifier_checkbox.style('color', 'white');
   magnifier_checkbox.input(uniformUpdate);
+
+  mask_checkbox = createCheckbox('mask', false);
+  mask_checkbox.position(10, 50);
+  mask_checkbox.style('color', 'white');
+  mask_checkbox.input(uniformUpdate);
+
+  luma_checkbox = createCheckbox('Luma', false);
+  luma_checkbox.position(10, 70);
+  luma_checkbox.style('color', 'white');
+  luma_checkbox.input(uniformUpdate);
+
+  roi_checkbox = createCheckbox('Region of interest', false);
+  roi_checkbox.position(10, 90);
+  roi_checkbox.style('color', 'white');
+  roi_checkbox.input(uniformUpdate);
+
   radius_slider = createSlider(0, 100, 20);
   radius_slider.position(10, 110);
   radius_slider.style('width', '80px');
-  // Masl selector
+
   sel = createSelect();
-  sel.position(500,10);
+  sel.position(10,10);
   sel.option('Gaussian blur');
-  sel.option('Sharpening');
   sel.option('Edges');
-  sel.option('Emboss');
   sel.input(uniformUpdate);
-  // Initialize mask
-  uniformUpdate();
-  
+
+  uniformUpdate(); 
 }
 
 function draw() {
   background(0);
   quad(-width / 2, -height / 2, width / 2, -height / 2,
         width / 2, height / 2, -width / 2, height / 2);
-  // Send mouse position and radius in every frame
   maskShader.setUniform('mouse', [mouseX/width, mouseY/height]);
-  maskShader.setUniform('actionRadius', radius_slider.value()/100.0);
 }
 
 function uniformUpdate(){
-  // Mask application
   maskShader.setUniform('apply_mask',mask_checkbox.checked());
-  // Luma application
   maskShader.setUniform('luma',luma_checkbox.checked());
-  // Mask selection
+  maskShader.setUniform('roi',roi_checkbox.checked());
+  maskShader.setUniform('magnifier',magnifier_checkbox.checked());
+
   if( sel.value() == 'Gaussian blur' ){
     maskShader.setUniform('mask', [1.0/16.0,2.0/16.0,1.0/16.0, 2.0/16.0,4.0/16.0,2.0/16.0, 1.0/16.0,2.0/16.0,1.0/16.0]);
-  }else if( sel.value() == 'Sharpening' ){
-    maskShader.setUniform('mask', [0.0,-1.0,0.0, -1.0,5.0,-1.0, 0.0,-1.0,0.0]);
   }else if( sel.value() == 'Edges' ){
     maskShader.setUniform('mask', [-1.0,-1.0,-1.0, -1.0,8.0,-1.0, -1.0,-1.0,-1.0,]);
-  }else if( sel.value() == 'Emboss' ){
-    maskShader.setUniform('mask', [-2.0,-1.0,0.0, -1.0,1.0,1.0, 0.0,1.0,2.0]);
   }
-  // Region of interest
-  maskShader.setUniform('roi',roi_checkbox.checked());
-  // Magnifier
-  maskShader.setUniform('magnifier',magnifier_checkbox.checked());
 }
